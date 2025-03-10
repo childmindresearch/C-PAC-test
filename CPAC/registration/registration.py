94bd58f9ca0255a15077658ed95ac15387b6d86f
birajstha:construction_worker::penguin <birajstha@gmail.com>
2025-03-10 18:20:20 -0400

    correcting the switch for mask_sbref

--- CPAC/registration/registration.py
+++ CPAC/registration/registration.py
@@ -3132,7 +3132,6 @@ def overwrite_transform_anat_to_template(wf, cfg, strat_pool, pipe_num, opt=None
             "coregistration",
             "func_input_prep",
             "mask_sbref",
-            "run",
         ],
     ],
     inputs=["sbref", "space-bold_desc-brain_mask"],
